5ad39e86b9470996e31b72a080e248c68cc1e9d9
Dario Lostumbo <dario.lostumbo@hotmail.com>
2022-03-10 00:08:54 +0100

    third commit

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -2,7 +2,7 @@ const userFake=require('../usersFake')
 
 module.exports=(app)=>{
 
-    app.get("/",(req,res)=>{
+    app.get("/api",(req,res)=>{
         console.log('INSIDE REQ')
 
         //const userSend=JSON.stringify(userFake)
